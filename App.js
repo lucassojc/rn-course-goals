a64b8766542d5c1f71122901ff400f1287664b2f
Luka <luka.jovanovic.1995@gmail.com>
2021-03-28 13:48:12 +0200

    Initial commit

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TextInput, View, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ padding: 30 }}>
+      <View>
+        <TextInput />
+        <Button title="ADD" />
+      </View>
+      <View></View>
     </View>
   );
 }
